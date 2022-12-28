5c3873ac8277c69c7fdb0ec339e2bde5b2fe6c50
Carlos Daniel <ruuufffaria@gmail.com>
2022-12-28 16:32:22 -0300

    feat: hover and active styles

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -3,7 +3,7 @@ export default function DeleteButton({ onClick }) {
     <button
       title="Deletar item"
       onClick={onClick}
-      className="bg-red-600 text-white relative flex items-center p-2 rounded-md w-7 h-7"
+      className="bg-red-400 hover:bg-red-600 text-white relative flex items-center p-2 rounded-md w-7 h-7 active:scale-95"
     >
       <div className="bg-current w-5 h-1 absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rotate-[45deg]"></div>
       <div className="bg-current w-5 h-1 absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rotate-[-45deg]"></div>
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -3,9 +3,9 @@ export default function SubmitButton({ label }) {
     <button
       type="submit"
       title={label}
-      className="bg-emerald-600 text-white flex items-center px-4 py-2 rounded-xl"
+      className="bg-emerald-500 hover:bg-emerald-600 font-semibold truncate text-white flex items-center px-4 py-2 rounded-xl active:scale-95"
     >
-      <span className="mr-2">{label}</span>
+      <span className="mr-2 small:hidden">{label}</span>
       <div className="relative inline-block w-6 h-6">
         <div className="bg-current w-5 h-1 absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rotate-[90deg]"></div>
         <div className="bg-current w-5 h-1 absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%]"></div>
