3b11c8f6fbc69bc15095c42794ff2e92d07535d7
Carlos Daniel <ruuufffaria@gmail.com>
2022-12-28 16:34:24 -0300

    min value to number input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,11 +5,9 @@ export default function Input({
   decimal = false,
   reference,
   placeholder,
-  pr,
-  pl,
 }) {
   return (
-    <div className={`flex flex-col ${pr ? "pr-2" : ""} ${pl ? "pl-2" : ""}`}>
+    <div className="flex flex-col">
       <label htmlFor={id} className="mb-1 font-semibold">
         {label}
       </label>
@@ -19,9 +17,14 @@ export default function Input({
         type={type}
         ref={reference}
         placeholder={placeholder}
-        {...(decimal && { step: ".01", pattern: "^d*(.d{0,2})?$" })}
+        {...(type === "number" && { min: "1" })}
+        {...(decimal && {
+          step: ".01",
+          pattern: "^d*(.d{0,2})?$",
+          min: "0.01",
+        })}
         required
-        className="border-2 border-emerald-300 rounded-md p-1 outline-none focus:border-emerald-700"
+        className="w-full border-2 border-emerald-300 rounded-md p-1 outline-none focus:border-emerald-700"
       />
     </div>
   );
