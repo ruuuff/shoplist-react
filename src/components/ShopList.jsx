b0c7ce962eff7a0535e0c99b641dbac098e8bea7
Carlos Daniel <ruuufffaria@gmail.com>
2022-12-28 16:35:36 -0300

    feat: framer-motion animations

--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,41 +1,83 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { formatCurrency } from "../utils";
 import DeleteButton from "./DeleteButton";
 
+const listVariants = {
+  hidden: {},
+  show: { transition: { staggerChildren: 0.1 } },
+};
+
+const itemVariants = {
+  hidden: { scale: 0.8, opacity: 0 },
+  show: { scale: 1, opacity: 1 },
+  exit: { scale: 0.6, opacity: 0 },
+};
+
 export default function ShopList({ shoplist, deleteItem }) {
-  return shoplist.length > 0 ? (
-    <ul className="bg-white rounded-xl tablet:mx-4">
-      {shoplist.map(({ id, item, amount, price }) => {
-        const priceBRL = formatCurrency(price);
-        const totalPriceBRL = formatCurrency(price * amount);
-        return (
-          <li
-            key={id}
-            className="flex gap-4 items-center p-4 border-t-4 border-t-emerald-300 first:border-none"
-          >
-            <div className="grid grid-cols-2 gap-2 flex-1">
-              {/* item name */}
-              <div className="col-span-2 font-semibold truncate first-letter:uppercase">
-                {item}
-              </div>
-              {/* amount */}
-              <div>
-                {amount > 1 ? priceBRL : ""} x {amount}
-              </div>
-              {/* total value */}
-              <div className="font-semibold">
-                {amount > 1 ? totalPriceBRL : priceBRL}
-              </div>
-            </div>
-            <div>
-              <DeleteButton onClick={() => deleteItem(id)} />
-            </div>
-          </li>
-        );
-      })}
-    </ul>
-  ) : (
-    <div className="bg-white rounded-xl tablet:mx-4">
-      <h3 className="text-3xl text-center py-4">Lista vazia</h3>
-    </div>
+  return (
+    <AnimatePresence mode="wait">
+      {shoplist.length > 0 ? (
+        <motion.ul
+          key="list"
+          initial="hidden"
+          animate="show"
+          exit={itemVariants.exit}
+          variants={listVariants}
+          className="space-y-1 rounded-xl tablet:mx-4"
+        >
+          <AnimatePresence mode="popLayout">
+            {shoplist.map(({ id, item, amount, price }) => (
+              <motion.li
+                key={id}
+                layout
+                variants={itemVariants}
+                exit={itemVariants.exit}
+                transition={{ type: "spring" }}
+                className="flex items-center p-4 gap-4 bg-white even:bg-emerald-50 first:rounded-t-xl last:rounded-b-xl transition-colors duration-300"
+              >
+                <div className="flex-1 grid grid-cols-2 gap-2">
+                  {/* item name */}
+                  <div className="col-span-2 font-semibold truncate first-letter:uppercase">
+                    <span title="Nome do item">{item}</span>
+                  </div>
+                  {/* amount */}
+                  <div>
+                    <span
+                      title={
+                        amount > 1 ? "Valor vezes quantidade" : "Quantidade"
+                      }
+                    >
+                      {amount > 1 ? formatCurrency(price) : ""} x {amount}
+                    </span>
+                  </div>
+                  {/* total value */}
+                  <div className="font-semibold truncate">
+                    <span title="Valor">
+                      {amount > 1
+                        ? formatCurrency(price * amount)
+                        : formatCurrency(price)}
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <DeleteButton onClick={() => deleteItem(id)} />
+                </div>
+              </motion.li>
+            ))}
+          </AnimatePresence>
+        </motion.ul>
+      ) : (
+        <motion.div
+          key="advice"
+          initial="hidden"
+          animate="show"
+          exit={itemVariants.exit}
+          variants={itemVariants}
+          className="bg-white rounded-xl tablet:mx-4"
+        >
+          <h3 className="text-3xl text-center py-4">Lista vazia</h3>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
